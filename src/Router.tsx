ba6abca5737e2cb205506a7f4edbe2fa2f699262
LucasSpinelli003 <lucaszk003@gmail.com>
2024-05-24 00:46:18 -0300

    wip: improving home page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import First from "./component/test";
-import { Home } from "./Pages/Home";
+import { Hometest } from "./Pages/Home/testHome";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hometest />} />
         <Route path="/pets" element={<First />} />
       </Route>
     </Routes>
