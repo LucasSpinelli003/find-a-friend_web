cdfc5ff1a1d7afa8274df4746b14cf24a38ce6f9
LucasSpinelli003 <lucaszk003@gmail.com>
2024-05-24 18:01:14 -0300

    feature: home page implemented and get method

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,105 +1,130 @@
 import logo from "../../assets/img/Group 19.png";
 import animals from "../../assets/img/OBJECTS.png";
+import searchButton from "../../assets/img/Group 20.png";
 
 import { Col, Row, Typography } from "antd";
-import { SelectedAcronymState } from "../../component/selectedAcronymState";
+import { SelectedAcronymStateAndCity } from "../../component/selectedAcronymStateAndCity";
+import { createUseStyles } from "react-jss";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = createUseStyles({
+  container: {
+    background: "#F15156",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    display: "flex",
+    flexDirection: "column",
+    width: "80vw",
+    gap: "7rem",
+    height: "77vh",
+  },
+  divHeaderCol: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  mainCol: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  textMainCol: {
+    color: "#fff",
+    fontSize: "5rem",
+    fontFamily: "Nunito",
+    fontWeight: 800,
+    fontStyle: "extraBold",
+    maxWidth: "29vw",
+    lineHeight: 1,
+  },
+  footerCol: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: "5rem",
+  },
+  textFooterCol: {
+    display: "flex",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "2rem",
+    fontFamily: "Nunito",
+    fontWeight: 600,
+  },
+  divFooterCol: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "2rem",
+    alignItems: "center   ",
+  },
+  searchButtonClass: {
+    height: "72px",
+    width: "72px",
+    background: "#F4D35E",
+    border: "none",
+    borderRadius: "18px",
+    cursor: "pointer",
+  },
+});
+
 export function Home() {
+  const {
+    container,
+    row,
+    divHeaderCol,
+    mainCol,
+    textMainCol,
+    footerCol,
+    textFooterCol,
+    divFooterCol,
+    searchButtonClass,
+  } = useStyles();
+
+  const navigate = useNavigate();
   return (
-    <section
-      style={{
-        background: "#F15156",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Row
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          width: "80vw",
-          height: "77vh",
-          justifyContent: "space-between",
-        }}
-      >
-        <Col
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "6rem",
-            flexDirection: "column",
-            maxWidth: "50%",
-          }}
-        >
-          <div>
+    <section className={container}>
+      <Row className={row}>
+        <Col>
+          <div className={divHeaderCol}>
             <img src={logo} alt="" />
           </div>
-          <Typography
-            style={{
-              color: "#fff",
-              fontSize: "6rem",
-              fontFamily: "Nunito",
-              fontWeight: 800,
-              fontStyle: "extraBold",
-              maxWidth: "86%",
-              lineHeight: 1,
-            }}
-          >
-            Leve a felicidade para o seu lar
-          </Typography>
-          <Typography
-            style={{
-              display: "flex",
-              alignItems: "center",
-              color: "#fff",
-              fontSize: "2rem",
-              fontFamily: "Nunito",
-              fontWeight: 600,
-              width: "65%",
-            }}
-          >
-            Encontre o animal de estimação ideal para seu estilo de vida!
-          </Typography>
         </Col>
-        <Col
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            maxHeight: "100%",
-            gap: "7rem",
-          }}
-        >
+        <Col className={mainCol}>
+          <Typography className={textMainCol}>
+            Leve <br />a felicidade para o seu lar
+          </Typography>
           <div>
             <img src={animals} alt="" />
           </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "row",
-              gap: "5rem",
-            }}
-          >
-            <Typography style={{ color: "#fff" }}>Busque um amigo:</Typography>
-            <div
-              style={{
-                display: "flex",
-                color: "#fff",
-                justifyContent: "center",
-                background: "transparent",
-                border: "1px solid #fff",
-                borderRadius: "10px",
-                maxWidth: "10%",
-                minHeight: "7%",
-                alignItems: "center",
+        </Col>
+        <Col className={footerCol}>
+          <div style={{ display: "flex", flexDirection: "row", width: "30vw" }}>
+            <Typography className={textFooterCol}>
+              Encontre o animal de estimação ideal para seu estilo de vida!
+            </Typography>
+          </div>
+          <div className={divFooterCol}>
+            <Typography style={{ color: "#fff", fontSize: "1rem" }}>
+              Busque um amigo:
+            </Typography>
+            <div>
+              <SelectedAcronymStateAndCity />
+            </div>
+            <button
+              className={searchButtonClass}
+              onClick={() => {
+                navigate("/pets");
               }}
             >
-              <SelectedAcronymState />
-            </div>
+              <img src={searchButton} alt="" />
+            </button>
           </div>
         </Col>
       </Row>
