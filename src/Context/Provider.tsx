41def020514c3845249a664124806eacb2f82f25
LucasSpinelli003 <lucaszk003@gmail.com>
2024-05-27 18:23:26 -0300

    wip: go to selected pet

--- src/Context/Provider.tsx
+++ src/Context/Provider.tsx
@@ -5,6 +5,8 @@ interface ContextProps {
   setCity: React.Dispatch<React.SetStateAction<string>>;
   preAlocatedCity: string;
   setPreAlocatedCity: React.Dispatch<React.SetStateAction<string>>;
+  actualId: string;
+  setActualId: React.Dispatch<React.SetStateAction<string>>;
 }
 
 interface ContextProviderProps {
@@ -16,10 +18,18 @@ export const Context = createContext({} as ContextProps);
 export function Provider({ children }: ContextProviderProps) {
   const [city, setCity] = useState("Cidade");
   const [preAlocatedCity, setPreAlocatedCity] = useState("Cidade");
+  const [actualId, setActualId] = useState("");
 
   return (
     <Context.Provider
-      value={{ city, setCity, preAlocatedCity, setPreAlocatedCity }}
+      value={{
+        city,
+        setCity,
+        preAlocatedCity,
+        setPreAlocatedCity,
+        actualId,
+        setActualId,
+      }}
     >
       {children}
     </Context.Provider>
--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./Pages/Home";
 import { PetsDashBoard } from "./Pages/Pets";
+import { PetInfo } from "./Pages/Pet";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/pet" element={<PetInfo />} />
         <Route path="/pets" element={<PetsDashBoard />} />
       </Route>
     </Routes>
